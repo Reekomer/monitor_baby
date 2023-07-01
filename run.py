a65c82f07734dcdce86a7a4671134600cd43c035
Jeney <jeremy.degail@gmail.com>
2023-07-01 03:41:10 +0100

    Improve Code

--- run.py
+++ run.py
@@ -3,23 +3,27 @@ import time
 import os
 
 from loguru import logger
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 from collections import Counter
 
 QUESTIONS = [
     "Can you see the baby's nose?",
-    "Can you see the baby's mouth?"
+    "Can you see the baby's mouth?",
+    "Is the baby awake?"
 ]
 ENDPOINT = os.getenv('ENDPOINT')
 
 def main():
     # prepare image + question
-    images = _fetch_images()
-    for image in images:
-        _check_images(image)
+    filename_to_bytes = _fetch_images()
+    for filename_to_byte in filename_to_bytes:
+        logger.info(f'Parsing: {filename_to_byte["filename"]} ...')
+        _check_images(filename_to_byte["bytes"])
 
 
 def _check_images(image):
+    results = []
     for question in QUESTIONS:
         payload = {
             'inputs': {
@@ -28,20 +32,40 @@ def _check_images(image):
                 }
         }
         response = requests.post(ENDPOINT, json = payload, headers={'Authorization': f'Bearer {os.getenv("TOKEN")}'})
-        result = response.json()[0]['answer']
-        answer_count = Counter(result)
-        if answer_count["yes"] != len(QUESTIONS):
-            _play_alarm()
+        answer = response.json()[0]['answer']
+        logger.info(f"{question}: {answer}")
+        if "Is the baby awake?" == question:
+            logger.info("Sleeping for 30 secs ...")
+            time.sleep(30)
+            continue
+        results.append(answer)
+
+    answer_count = Counter(results)
+    if answer_count["yes"] != len(QUESTIONS):
+        logger.warning("Baby has an issue!")
+        _play_alarm()
+    else:
+        logger.success("Baby is ok!")
 
 
-def _fetch_images():
-    with open('./images/th-483835542', mode='rb') as file:
-        return str(bytearray(file.read()))
+def _fetch_images() -> dict:
+    images = []
+    image_names = os.listdir('images')
+    for image_name in image_names:
+        with open(f'./images/{image_name}', mode='rb') as file:
+            data = {
+                "filename": image_name,
+                "bytes": str(bytearray(file.read()))
+            }
+            images.append(data)
+    return images
 
 def _play_alarm():
-    playsound('./sounds/mixkit-critical-alarm-1004.wav')
+    song = AudioSegment.from_wav("sounds/mixkit-emergency-alert-alarm-1007.wav")
+    # play(song)
 
 if __name__ == '__main__':
     while True:
         main()
+        logger.info("Sleeping for 10 secs ...")
         time.sleep(10)
